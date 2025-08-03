ba856647610aaf5c87e706594eb16deeb8c776ba
Dylan Lawton <dylanlawton@live.co.uk>
2025-08-03 09:34:36 +0100

    commit

--- az_reader.py
+++ az_reader.py
@@ -1,8 +1,22 @@
-from azure.identity import DefaultAzureCredential
+# az_reader.py
+from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 
 def list_resource_groups(subscription_id: str):
-    credential = DefaultAzureCredential()
+    credential = AzureCliCredential()
     client = ResourceManagementClient(credential, subscription_id)
     groups = client.resource_groups.list()
-    return [g.name for g in groups]
+
+    group_data = []
+    for group in groups:
+        resources = list(client.resources.list_by_resource_group(group.name))
+        tags = group.tags if group.tags else {}
+
+        group_data.append({
+            "name": group.name,
+            "location": group.location,
+            "resource_count": len(resources),
+            "tags": tags
+        })
+
+    return group_data
